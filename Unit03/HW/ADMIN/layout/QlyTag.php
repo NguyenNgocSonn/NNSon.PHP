38cd6659f2dd9b17da9215bfd28e5866e63470e7
NguyenNgocSonn <n.n.son.it.cv@gmail.com>
2018-12-15 16:56:36 +0700

    asdad

---
+++ Unit03/HW/ADMIN/layout/QlyTag.php
@@ -0,0 +1,22 @@
+<?php
+require_once('header.php');
+?>
+
+<div class="container-fluid">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h1 class="page-header">Quản lý tag</h1>
+                    </div>
+                    <!-- /.col-lg-12 -->
+                </div>
+                <!-- /.row -->
+            </div>
+            <!-- /.container-fluid -->
+        </div>
+        <!-- /#page-wrapper -->
+
+    </div>
+
+<?php
+require_once('footer.php');
+?>
